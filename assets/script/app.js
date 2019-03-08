6f9f78c6a26aee9e54bd3b96bad03693082d0b61
gthiffault <gabriel.thiffault@gmail.com>
2019-03-07 22:47:51 -0500

    Assets v26

--- assets/script/app.js
+++ assets/script/app.js
@@ -1986,28 +1986,50 @@ if($('.c-contact-us').length) {
             		var marker001 = [],
             		infoWindowContent = [];
 
-        var locations = [['Fernes Valens', 45.089616, -74.177272]];
-var markerL, i;
-        for (i = 0; i < locations.length; i++) {  
-            var image = {
-                scaledSize: new google.maps.Size(51, 78),
-                url: "https://staging-valens.agencezel.dev/assets/images/icons/fermes-valens_shop.svg"
-            };
-            markerL = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-                map: map
-                ,icon: image,
-                optimized: false,
-                animation: CurrentAnimation
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-            });
-            google.maps.event.addListener(markerL, 'click', (function(markerL, i) {
-            return function() {
-                infowindow.setContent(locations[i][0]);
-            }
-            })(markerL, i));
-        }
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
 
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124},
+        {lat: -33.848588, lng: 151.209834},
+        {lat: -33.851702, lng: 151.216968},
+        {lat: -34.671264, lng: 150.863657},
+        {lat: -35.304724, lng: 148.662905},
+        {lat: -36.817685, lng: 175.699196},
+        {lat: -36.828611, lng: 175.790222},
+        {lat: -37.750000, lng: 145.116667},
+        {lat: -37.759859, lng: 145.128708},
+        {lat: -37.765015, lng: 145.133858},
+        {lat: -37.770104, lng: 145.143299},
+        {lat: -37.773700, lng: 145.145187},
+        {lat: -37.774785, lng: 145.137978},
+        {lat: -37.819616, lng: 144.968119},
+        {lat: -38.330766, lng: 144.695692},
+        {lat: -39.927193, lng: 175.053218},
+        {lat: -41.330162, lng: 174.865694},
+        {lat: -42.734358, lng: 147.439506},
+        {lat: -42.734358, lng: 147.501315},
+        {lat: -42.735258, lng: 147.438000},
+        {lat: -43.999792, lng: 170.463352}
+      ]
+            		
 
 			        /* ==================================================================
 			           #ADDMARKER
